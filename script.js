e0aab426fe9217b13313614a5a862e2ceed16a83
GenLI7723 <ge75pax@mytum.de>
2025-07-17 14:56:38 +0200

    test new front page blue print

---
+++ script.js
@@ -0,0 +1,14 @@
+/* script.js */
+function openTab(evt, tabName) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
